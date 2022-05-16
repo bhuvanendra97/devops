079de7d07c72d6b067bbc1ac51add3dd8fac9309
bhuvanendra97 <49412683+bhuvanendra97@users.noreply.github.com>
2022-05-16 23:55:11 +0530

    health check api added

--- src/main/java/com/devops/learning/DevopsApplication.java
+++ src/main/java/com/devops/learning/DevopsApplication.java
@@ -2,12 +2,16 @@ package com.devops.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class DevopsApplication {
 
 	public static void main(String[] args) {
@@ -17,5 +21,15 @@ public class DevopsApplication {
 	@GetMapping("/get/{message}")
 	public String getMessage(@PathVariable String message) {
 		return message;
+	}
+	
+	@PostMapping("/getBody")
+	public String getBody(@RequestBody String message) {
+		return message;
+	}
+	
+	@GetMapping("/health")
+	public String health() {
+		return "Hello";
 	}
 }
